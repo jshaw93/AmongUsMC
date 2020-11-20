cb3ca56b9de0a5e1e8c812d5ff2d4ab85e77d9ca
Tyrrel <jshaw93@gmail.com>
2020-11-20 16:53:45 -0700

    Semi-complete Vent Cleaning task

--- src/me/twoeggs/aumc/tasks/VentCleanTask.java
+++ src/me/twoeggs/aumc/tasks/VentCleanTask.java
@@ -17,23 +17,37 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 
-public class TestTask implements Listener, CommandExecutor {
+public class VentCleanTask implements CommandExecutor, Listener {
 
     private final Main instance;
 
     private final int[] indexes = new int[] {10, 11, 12, 14, 15, 16, 28, 29, 30, 32, 33, 34};
 
-    public TestTask(Main instance) {
+    public VentCleanTask(Main instance) {
         this.instance = instance;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        if(s.equalsIgnoreCase("testtask")) {
+        if(s.equalsIgnoreCase("venttask")) {
             if(!(sender instanceof Player)) return true;
             Player player = (Player)sender;
-
-            openGUI(player, inventory());
+            Inventory inv = inventory();
+            ItemStack leaves = new ItemStack(Material.OAK_LEAVES);
+            int count = 0;
+            for(int index : indexes) {
+                int x = new Random().nextInt(2);
+                if(x == 0) {
+                    inv.setItem(index, leaves);
+                    count++;
+                }
+            }
+            if(count == 0) {
+                for(int index : indexes) {
+                    inv.setItem(index, leaves);
+                }
+            }
+            openGUI(player, inv);
             return true;
         }
         return false;
@@ -60,9 +74,30 @@ public class TestTask implements Listener, CommandExecutor {
     }
 
     private Inventory inventory() {
-        Inventory inv = Bukkit.createInventory(null, 45, ChatColor.DARK_BLUE+"yeet");
+        Inventory inv = Bukkit.createInventory(null, 45, ChatColor.DARK_BLUE+"Vent Cleaning Duty");
         // Top bar
         for(int i = 0; i < 9; i++) {
+            inv.setItem(i, blackGlassPane());
+        }
+        // Left bar
+        for(int i = 9; i < 37; i+=9) {
+            inv.setItem(i, blackGlassPane());
+        }
+        // Middle bars
+        // Horizontal
+        for(int i = 19; i < 27; i++) {
+            inv.setItem(i, blackGlassPane());
+        }
+        // Vertical
+        for(int i = 4; i < 45; i+=9) {
+            inv.setItem(i, blackGlassPane());
+        }
+        // Right bar
+        for(int i = 8; i < 37; i+=9) {
+            inv.setItem(i, blackGlassPane());
+        }
+        //Bottom bar
+        for(int i = 37; i < 45; i++) {
             inv.setItem(i, blackGlassPane());
         }
         return inv;
@@ -70,12 +105,26 @@ public class TestTask implements Listener, CommandExecutor {
 
     @EventHandler
     public void onClick(InventoryClickEvent e) {
-        if(!ChatColor.stripColor(e.getView().getTitle()).equalsIgnoreCase("yeet")) return;
+        if(!ChatColor.stripColor(e.getView().getTitle()).equalsIgnoreCase("Vent Cleaning Duty")) return;
         if(e.getCurrentItem() == null) return;
         if(!e.getCurrentItem().isSimilar(new ItemStack(Material.OAK_LEAVES))) {
             e.setCancelled(true);
             return;
         }
         Player player = (Player)e.getWhoClicked();
+        int index = e.getSlot();
+        Inventory inv = e.getClickedInventory();
+        if(inv == null) return;
+        inv.setItem(index, null);
+        player.playSound(player.getLocation(), Sound.BLOCK_CROP_BREAK, 1.0f, 1.0f);
+        int count = 0;
+        for(int i : indexes) {
+            if(inv.getItem(i) != null) {
+                count++;
+            }
+        }
+        if(count == 0) {
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, player::closeInventory, 20);
+        }
     }
 }
